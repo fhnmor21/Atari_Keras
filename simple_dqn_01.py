2675527472af4f8de44b6391cea3dd7a20a8ad48
Flavio Moreira <flaviom@bigworldtech.com>
2019-02-28 16:09:19 +1100

    fix small issues with the player

--- simple_dqn_01.py
+++ simple_dqn_01.py
@@ -1,54 +1,107 @@
 #! python
 
+# import random
 from time import sleep
 from collections import deque
 import gym
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras import backend as K
+import tensorflow as tf
 
 class AtariPlayer:
     # ctor
     def __init__(self, gameName = 'Riverraid-ram-v0'):
         self.__env = gym.make(gameName)
-        self.__currentEnviron = self.__env.reset()
-        self.__memory = deque(self.__currentEnviron)
+        self.__currentState = self.__env.reset()
+        self.__memory = deque()
 
     # add current 
-    def __memorize(self):
-        self.__memory.append(self.__currentEnviron)
+    def __memorize(self, action, reward, next_state, done, info):
+        self.__memory.append( (self.__currentState, action, reward, done, info, next_state) )
+
+    def stateSize(self):
+        return len()
 
     # environment description
     def printEnvironment(self):
-        print(self.__currentEnviron)
+        print(self.__currentState)
         print(self.__env.action_space)
         print(self.__env.observation_space)
 
-    # perfomr a play sessions
+    # perform a play session
     def runSession(self, maxSteps, shouldRender = False):
         for _ in range(maxSteps):
             self.__env.step(self.__env.action_space.sample())
-            # next_state, reward, done, info
-            self.__currentEnviron = self.__env.step(self.__env.action_space.sample())
+            # state, reward, done, info
+            action = self.__env.action_space.sample()
+            next_state, reward, done, info =self.__env.step(action)
+            self.__memorize(action, reward, next_state, done, info)
 
-            if self.__currentEnviron[2] == True:
+            if done == True:
                 break
             else:
-                self.__memorize()
+                # update __currentState
+                self.__currentState = next_state
 
-            if shouldRender:
-                self.__env.render('human')
-                print(self.__currentEnviron)
-                sleep(0.034)
+                # render if needed
+                if shouldRender:
+                    self.__env.render('human')
+                    print("Current State: ")
+                    print(self.__memory[-1])
+                    sleep(0.034)
 
         return self.__memory
 
     # current environment data
-    def getCurrentEnvironment(self):
-        return self.__currentEnviron
+    def getCurrentObservations(self):
+        memLen = len(self.__memory)
+        if memLen > 0:
+            return self.__memory[-1]
+        else:
+            return None
 
     # close enrironment
     def shutdown(self):
         self.__env.close()
 
+class DQNet:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+
+    def _huber_loss(self, y_true, y_pred, clip_delta=1.0):
+        """Huber loss for Q Learning
+        References: https://en.wikipedia.org/wiki/Huber_loss
+                    https://www.tensorflow.org/api_docs/python/tf/losses/huber_loss
+        """
+
+        error = y_true - y_pred
+        cond  = K.abs(error) <= clip_delta
+
+        squared_loss = 0.5 * K.square(error)
+        quadratic_loss = 0.5 * K.square(clip_delta) + clip_delta * (K.abs(error) - clip_delta)
+
+        return K.mean(tf.where(cond, squared_loss, quadratic_loss))
+
+    def _build_model(self):
+        # Neural Net for Deep-Q learning Model
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss=self._huber_loss,
+                      optimizer=Adam(lr=self.learning_rate))
+        return model
+
+
+
+
 if __name__ == '__main__':
     rr = AtariPlayer()
-    rr.runSession(5000, True)
-    pass
+    rr.runSession(1000, True)
+    # rr.printEnvironment()
+    # print(rr.getCurrentObservations())
+    rr.shutdown()
